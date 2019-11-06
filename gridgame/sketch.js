0fc7ba4bbf2805d6f4cfc1418b4e20f01eca2553
mueezrafiquie <mueezalba@gmail.com>
2019-11-06 14:12:09 -0600

    got border and snake eats food and it reappears

--- gridgame/sketch.js
+++ gridgame/sketch.js
@@ -6,7 +6,7 @@ let cols = 30;
 
 let playerX = 10;
 let playerY = 15;
-let playerPosition;
+
 let speed = 1;
 
 let movingLeft = false;
@@ -14,41 +14,22 @@ let movingRight = false;
 let movingUp = false;
 let movingDown = false;
 
-let food;
 let foodX;
 let foodY;
 
-let array = [
-  5,
-  6,
-  7,
-  8,
-  9,
-  10,
-  15,
-  16,
-  17,
-  18,
-  19,
-  20,
-  21,
-  22,
-  23,
-  24,
-  25
-];
+let array = [5, 6, 7, 8, 9, 10, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 
 function setup() {
   if (windowWidth > windowHeight) {
-    createCanvas(windowHeight, windowHeight);
+    createCanvas(windowHeight * 0.9, windowHeight * 0.9);
   } else {
-    createCanvas(windowWidth, windowWidth);
+    createCanvas(windowWidth * 0.9, windowWidth * 0.9);
   }
   grid = createEmptyGrid(cols, rows);
 
+  createBorder();
 
   grid[playerY][playerX] = 1;
-  playerPosition = playerY, playerX;
   foodX = random(array);
   foodY = random(array);
 }
@@ -62,9 +43,49 @@ function draw() {
 
 function windowResized() {
   if (windowWidth > windowHeight) {
-    createCanvas(windowHeight, windowHeight);
+    createCanvas(windowHeight * 0.9, windowHeight * 0.9);
   } else {
-    createCanvas(windowWidth, windowWidth);
+    createCanvas(windowWidth * 0.9, windowWidth * 0.9);
+  }
+}
+
+function createEmptyGrid() {
+  let emptyGrid = [];
+  for (let x = 0; x < cols; x++) {
+    emptyGrid.push([]);
+    for (let y = 0; y < rows; y++) {
+      emptyGrid[x].push(0);
+    }
+  }
+  return emptyGrid;
+}
+
+function displayGrid(grid, rows, cols) {
+  let cellSize = width / cols;
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (grid[y][x] === 0) {
+        fill(255);
+      } else {
+        fill(0);
+      }
+      rect(x * cellSize, y * cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+function createBorder() {
+  for (let i = 0; i < rows; i++) {
+    grid[0][i] = 1;
+  }
+  for (let j = 0; j < cols; j++) {
+    grid[j][0] = 1;
+  }
+  for (let j = cols - 1; j >= 0; j--) {
+    grid[rows - 1][j] = 1;
+  }
+  for (let j = cols - 1; j >= 0; j--) {
+    grid[j][rows - 1] = 1;
   }
 }
 
@@ -96,61 +117,36 @@ function keyPressed() {
 }
 
 function moveSnake() {
-  grid[playerY][playerX] = 0;
-
   // move the player
-  if (movingUp && playerY > 0) {
-    playerY -= 1;
-  }
-  if (movingDown && playerY < rows - 1) {
-    playerY += 1;
-  }
-  if (movingRight && playerX < cols - 1) {
-    playerX += 1;
-  }
-  if (movingLeft && playerX > 0) {
-    playerX -= 1;
-  }
+  if (frameCount % 5 === 0) {
+    grid[playerY][playerX] = 0;
 
-  // put player back into grid
-  grid[playerY][playerX] = 1;
-}
-
-function createEmptyGrid() {
-  let emptyGrid = [];
-  for (let x = 0; x < cols; x++) {
-    emptyGrid.push([]);
-    for (let y = 0; y < rows; y++) {
-      emptyGrid[x].push(0);
+    if (movingUp && playerY > 1) {
+      playerY -= 1;
     }
-  }
-  return emptyGrid;
-}
-
-function displayGrid(grid, rows, cols) {
-  let cellSize = width / cols;
-  for (let y = 0; y < rows; y++) {
-    for (let x = 0; x < cols; x++) {
-      if (grid[y][x] === 0) {
-        fill(255);
-      } else {
-        fill(0);
-      }
-      rect(x * cellSize, y * cellSize, cellSize, cellSize);
+    if (movingDown && playerY < rows - 2) {
+      playerY += 1;
     }
+    if (movingRight && playerX < cols - 2) {
+      playerX += 1;
+    }
+    if (movingLeft && playerX > 1) {
+      playerX -= 1;
+    }
+    // put player back into grid
+    grid[playerY][playerX] = 1;
   }
 }
-
 
 function displayFood() {
   grid[foodY][foodX] = 1;
 
-  food = foodY, foodX;
-  
-  if (food === playerPosition) {
-    console.log("yup")
-  }
-  else {
-
+  if (foodY === playerY && foodX === playerX) {
+    console.log("yup");
+    grid[foodY][foodX] = 0;
+    foodX = random(array);
+    foodY = random(array);
+    grid[foodY][foodX] = 1;
+  } else {
   }
 }
