723f930c56d45ce7d903cf6f59ae17d0d4c1584f
mueezrafiquie <mueezalba@gmail.com>
2019-12-13 14:11:56 -0600

    /

--- messingwithstuff/sketch.js
+++ messingwithstuff/sketch.js
@@ -1,8 +1,15 @@
 let hit = false;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+}
+
 function draw() {
   background(255);
-	rect(200,200,100,150);
-	ellipse(mouseX,mouseY,100,100);
+  fill(180)
+  rect(200,200,100,150);
+  fill(0)
+  ellipse(mouseX,mouseY,100,100);
 
 	hit = collideRectCircle(200,200,100,150,mouseX,mouseY,100);
 
